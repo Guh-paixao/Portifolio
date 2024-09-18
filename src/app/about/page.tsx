b0c99bd94d0ed958584e6bb80b40d37f39df8c16
Guh-paixao <gustavopaixao.dev@gmail.com>
2024-09-18 18:08:01 -0300

    Refactor layout.tsx to update text styles and add leading-loose class

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,7 @@ import dev from '@/assets/images/PNGs/dev.jpg'
 import xPhoto from '@/assets/images/SVGs/x_photo.svg'
 import gsap from "gsap";
 import LinksMobile from "@/components/externalButtons/linksMobile";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function About() {
 
@@ -16,32 +17,38 @@ export default function About() {
     useEffect(() => {
         const tl = gsap.timeline()
         tl.to(portraitRef.current, { opacity: 1, x: 10, duration: 0.5 })
+        tl.to(linksRef.current, { opacity: 1, x: -10, duration: 0.7, })
         tl.to(textRef.current, { opacity: 1, x: -10, duration: 1, })
-        tl.to(linksRef.current, { opacity: 1, x: -10, duration: 1, })
     }, [])
 
     return (
         <div className="flex flex-row max-xl:flex-col flex-1 xl:ml-48 2xl:ml-52 items-center gap-6">
             <div className="flex flex-row max-xl:flex-col items-start gap-10 2xl:gap-14">
-                <div ref={portraitRef} className="flex flex-col opacity-0 bg-white 2xl:w-[230px] h-72 mt-2 border-t-4 items-center justify-center select-none p-4 pt-4 2xl:gap-4 gap-2 border-zinc-600 border-2 dark:border-0">
-                    <Image src={dev} alt="Developer" className="w-52" />
-                    <div className="flex flex-row items-center justify-center 2xl:gap-8 xl:gap-10">
+                <div ref={portraitRef} className="flex flex-col opacity-0 bg-white max-xl:w=[195px] 2xl:w-[230px] max-xl:h-60 h-72 mt-2 border-t-4 items-center justify-center select-none p-4 pt-4 2xl:gap-4 gap-2 border-zinc-600 border-2 dark:border-0">
+                    {dev ? <Image src={dev} alt="Developer" className="w-52 max-xl:w-40" />
+
+                        :
+
+                        <Skeleton className="w-52 max-xl:w-40 bg-slate-400 h-full" />
+                    }
+                    <div className="flex flex-row items-center justify-center gap-4 2xl:gap-8 xl:gap-10">
                         <a href="https://instagram.com/_gus_paixao" target="_blank" rel="noopener noreferrer">
                             <span className="flex font-rougeScript text-lg 2xl:pl-8 xl:pl-10 text-black -rotate-6 opacity-80">@_gus_paixao</span>
                         </a>
-                        <Image src={xPhoto} alt="X" width={30} className="opacity-80 pointer-events-none" />
+                        <Image src={xPhoto} alt="X" className="opacity-80 max-xl:w-6 w-8 pointer-events-none" />
                     </div>
                 </div>
+
+                <div ref={linksRef} className="max-xl:flex hidden max-xl:visible opacity-0 self-start">
+                    <LinksMobile />
+                </div>
+
                 <div ref={textRef} className="flex opacity-0 justify-center items-center">
-                    <p className="font-poppins text-justify text-dark dark:text-white text-base 2xl:text-lg text-pretty w-6/6 xl:w-72 2xl:w-80 leading-loose">
+                    <p className="font-poppins text-justify text-dark dark:text-white text-base 2xl:text-lg text-pretty w-11/12 xl:w-72 2xl:w-80 leading-loose">
                         Sou um desenvolvedor apaixonado por criar experiências únicas, tanto na web quanto no mundo dos jogos. Com uma formação em Engenharia da Computação, sempre fui movido pela curiosidade e pela vontade de transformar ideias em realidade. <br />
                         <span className="flex mt-4"> Nos últimos tempos, tenho me dedicado ao desenvolvimento de jogos, onde encontrei uma forma de unir minha paixão por programação com a criatividade do design. Meu sonho é publicar um jogo de sucesso na Steam, algo que reflita todo o esforço e a imaginação que coloco em cada projeto.</span>
                     </p>
                 </div>
-            </div>
-
-            <div ref={linksRef} className="max-xl:flex hidden max-xl:visible opacity-0 self-start">
-                <LinksMobile />
             </div>
         </div>
     )
